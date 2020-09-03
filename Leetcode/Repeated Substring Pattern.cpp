543af1a3fb464c537c5954f9e203676cadab6236
Avibtb <38174200+Avibtb@users.noreply.github.com>
2020-09-03 20:41:01 +0545

    Add files via upload

---
+++ Leetcode/Repeated Substring Pattern.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+         string str=s+s;
+        return str.substr(1,str.size()-2).find(s)!=string::npos;
+        
+    }
+};
