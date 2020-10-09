67d1024485c9afe633b4bd460497fa2e350c8014
Avibtb <38174200+Avibtb@users.noreply.github.com>
2020-10-09 19:25:28 +0545

    Add files via upload

---
+++ Leetcode/Serialize and Deserialize BST.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return makeString(root, new StringBuffer()).toString();
+    }
+    
+    public StringBuffer makeString(TreeNode root, StringBuffer sb) {
+        if(root == null) return new StringBuffer(" ");
+        if(root != null) {
+            sb.append(root.val);
+            sb.append(" ");
+            makeString(root.left, sb);
+            makeString(root.right, sb);
+        }
+        return sb;
+    }
+    
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data.equals(" ")) return null;
+        String[] str = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(str[0]));
+        for(int i=1; i<str.length; i++) {
+            TreeNode curr = root;
+            int val = Integer.parseInt(str[i]);
+            while(true) {
+                if(curr.val > val) {
+                    if(curr.left != null) {
+                        curr = curr.left;
+                    } else {
+                        curr.left = new TreeNode(val);
+                        break;
+                    }
+                } else { 
+                    if(curr.right != null) {
+                        curr = curr.right;
+                    } else {
+                        curr.right = new TreeNode(val);
+                        break;
+                    }
+                }
+            }
+        }
+        return root;
+    }
+}
