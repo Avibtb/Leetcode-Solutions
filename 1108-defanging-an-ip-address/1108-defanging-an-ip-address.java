0eac34695e28e920d8ed67f9efb5c088dd9f9b1b
Avibtb <38174200+Avibtb@users.noreply.github.com>
2022-02-19 00:14:19 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (23.55%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String defangIPaddr(String address) {
+         StringBuilder str = new StringBuilder();
+        //String str = "";
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i) == '.'){
+                str.append("[.]");
+            }
+            else{
+                str.append(address.charAt(i));
+            }
+        }
+        return str.toString();
+    }
+}
