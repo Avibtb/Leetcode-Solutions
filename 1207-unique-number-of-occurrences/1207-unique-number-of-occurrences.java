34d9e9c6f7bc827f33559067ed87b3ef8339a93e
Avibtb <38174200+Avibtb@users.noreply.github.com>
2022-02-22 20:52:13 +0530

    Time: 2 ms (75.91%), Space: 42.5 MB (12.56%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int[] c = new int[2001];
+        for (int n : arr) {
+            c[n+1000]++;   
+        }
+        Set<Integer> set = new HashSet();
+        for (int count : c) {
+            if (count == 0)
+                continue;
+            if (!set.add(count))
+                return false;
+        }
+        return true;
+        
+    }
+}
