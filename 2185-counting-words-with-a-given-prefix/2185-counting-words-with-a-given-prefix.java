6d210cf5579e9367beba4206b6e242c7dcd08691
Avibtb <38174200+Avibtb@users.noreply.github.com>
2022-03-11 23:16:41 +0530

    Time: 0 ms (100.00%), Space: 43 MB (82.35%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int c = 0;
+    for(String s : words) {
+        if(s.indexOf(pref)==0) 
+            c++;
+    }
+    return c;
+        
+    }
+}
