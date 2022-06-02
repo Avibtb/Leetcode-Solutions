d25a7defa15e6da31816eebc90ec5270232e2249
Avibtb <38174200+Avibtb@users.noreply.github.com>
2022-06-02 21:10:19 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (37.71%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-    	int n = nums.size(), k, l;
-    	for (k = n - 2; k >= 0; k--) {
-            if (nums[k] < nums[k + 1]) {
+        
+        // size of nums
+        int size = nums.size();
+        
+        // If size is 1 then do nothing
+        if(size == 1)
+            return;
+        
+        // Finding longest increasing subarray from the back
+        
+        int i;
+        for(i = size -2 ; i>=0;i--)
+        {
+            if(nums[i] >=nums[i+1])continue;
+            else
                 break;
-            }
         }
-    	if (k < 0) {
-    	    reverse(nums.begin(), nums.end());
-    	} else {
-    	    for (l = n - 1; l > k; l--) {
-                if (nums[l] > nums[k]) {
-                    break;
-                }
-            } 
-    	    swap(nums[k], nums[l]);
-    	    reverse(nums.begin() + k + 1, nums.end());
+
+        // If the entire array is in descending order
+        if(i<0) {
+            reverse(nums.begin(),nums.end());
+            return;
         }
+        
+        // Finding next greater element than nums[i]
+        int j = i+1;
+        while(j<size && nums[j]>nums[i])
+        {
+            j++;
+        }
+        
+        j--;  // Now j is pointing to the element just greater than nums[i]
+        
+        swap(nums[i],nums[j]);
+        reverse(nums.begin()+(i+1),nums.end());
+
+        return ;
+        
+        
     }
-}; 
+};
