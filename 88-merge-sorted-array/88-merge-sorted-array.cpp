b27261963b709223b24647169b300963545ee3f1
Avibtb <38174200+Avibtb@users.noreply.github.com>
2022-06-07 13:31:41 +0530

    Time: 4 ms (49.07%), Space: 9.2 MB (30.68%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int low = m-1, high = n-1, i = m+n-1;
+        while(high>=0){
+            if(low>=0 && nums1[low] > nums2[high]){
+                nums1[i--] = nums1[low--];
+            }
+            else{
+                nums1[i--] = nums2[high--];
+            }
+        }
+        
+    }
+};
